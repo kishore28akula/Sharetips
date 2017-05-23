6e8be349ebc4636ec386474d5862e1cdaceabceb
ashok285 <ashokkunam@gmail.com>
2017-05-23 17:46:50 +0530

    Ashok

---
+++ app/src/main/java/sharetips/test/venkatkishore/sharetips/Disclamer.java
@@ -0,0 +1,28 @@
+package sharetips.test.venkatkishore.sharetips;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Disclamer extends Fragment {
+
+    Button btnk;
+    public Disclamer() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_disclamer, container, false);
+
+//j
+        return view;
+    }
+}
