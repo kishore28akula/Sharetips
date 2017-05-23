1c6c5472ad6b6317c8d3e06fe943ade8bbf81337
venkatakishore <kishore28ec@gmail.com>
2017-05-23 14:50:46 +0530

    Splashchanges

--- app/src/main/java/sharetips/test/venkatkishore/sharetips/SharePageDetails.java
+++ app/src/main/java/sharetips/test/venkatkishore/sharetips/SharePageDetails.java
@@ -9,7 +9,6 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -210,7 +209,6 @@ public class SharePageDetails extends AppCompatActivity implements View.OnClickL
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.i("kishore","destroy");
     }
 
 }
